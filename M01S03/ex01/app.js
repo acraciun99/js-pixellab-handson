38631be9e4a3a13b2649a4ff99ff93497c3074a4
acraciun99 <87021081+acraciun99@users.noreply.github.com>
2022-07-11 21:59:10 +0300

    js curs

---
+++ M01S03/ex01/app.js
@@ -0,0 +1,36 @@
+var object = {
+  name: 'Dragos',
+  surname: 'Iordache',
+  age: 32,
+};
+
+console.warn(
+  `Afiseaza fraza “Ma numesc xxx yyy si am xx ani.”
+  in consola folosind operatorul de concatenare`,
+);
+
+console.log(
+  'Ma numesc ' +
+    object.name +
+    ' ' +
+    object.surname +
+    ' si am ' +
+    object.age +
+    ' ani.',
+);
+
+console.warn(`Afiseaza anul in care te-ai nascut scazand
+varsta pe care ai trecut-o in obiect din anul curent
+ (un proces numit hardcodare)
+`);
+console.log((2022 - object.age).toString());
+
+console.warn(
+  `Afiseaza fraza “xxx yyy are xx ani.”
+  in consola folosind operatorul de concatenare
+  `,
+);
+
+console.log(
+  object.name + ' ' + object.surname + ' are ' + object.age + ' ani.',
+);
