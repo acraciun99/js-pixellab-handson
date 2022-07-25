52bd3def3c81aa754e5be7cc65b865102b800bb0
acraciun99 <87021081+acraciun99@users.noreply.github.com>
2022-07-25 21:55:56 +0300

    js so4 started

---
+++ M01S04/ex01/app.js
@@ -0,0 +1,30 @@
+var value = window.prompt('Introdu o valoare');
+var message = '';
+var number = window.prompt('Cu ce numar compar?');
+
+value = Number(value);
+number = Number(number);
+
+if (isNaN(value) === true) {
+  message = `Nu ai introdus un numar`;
+} else if (value === number) {
+  message = `Numarul  ${value} este egal cu ${number}`;
+} else if (value > number) {
+  message = `Numarul ${value} este mai mare decat ${number}`;
+} else {
+  message = `Numarul ${value} este mai mic decat ${number}`;
+}
+
+var messageParagraph = document.getElementById('message');
+messageParagraph.innerText = message;
+
+// console.log('Valoarea este: ' + value);
+
+// if (value % 2 === 0) {
+//   message = 'Numarul este par';
+// } else {
+//   message = 'Numarul este impar';
+// }
+
+// console.log(message);
+// messageParagraph.innerText = message;
---
+++ M01S04/ex05/app.js
@@ -0,0 +1,10 @@
+var number = prompt('Introdu un numar');
+
+for (var i = 0; i <= 100; i++) {
+  console.log(i);
+  if (i % number !== 0) {
+    continue;
+  }
+
+  console.log(`Acest numar este multiplu de ${number}.`);
+}
