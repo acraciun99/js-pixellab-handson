f13be774ef7836299db82ea395b425a85b7480f7
acraciun99 <87021081+acraciun99@users.noreply.github.com>
2022-07-27 21:56:49 +0300

    Curs 27/7

---
+++ M01S04/ex08/app.js
@@ -0,0 +1,20 @@
+// var i = 0;
+
+// while (i <= 100) {
+//   console.log(i);
+
+//   i++;
+// }
+
+var i = 1;
+
+while (i < 100) {
+  if (i > 50) {
+    i++; //<- important
+
+    break;
+  }
+  console.log(i);
+
+  i++;
+}
