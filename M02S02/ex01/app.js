407b09c0a9b11fb657b7dd563c24f6f19308f588
acraciun99 <87021081+acraciun99@users.noreply.github.com>
2022-08-17 22:23:25 +0300

    bb

--- M02S02/ex01/app.js
+++ M02S02/ex01/app.js
@@ -3,4 +3,32 @@
 // p.id = 'myParagraph';
 // p.classList.add('clase', 'de', 'css');
 
-// document.body.appendChild(p);
+// document.body.append(p);
+
+const $p = $('<p>', {
+  text: 'Textul care trebuie sa apara in paragraf',
+  id: 'myParagraph',
+  class: 'clase de css',
+});
+const $msg = $('<p>', {
+  id: 'message',
+  text: 'parola nu trebuie sa fie mai mica de 5 caractere',
+});
+
+const $container = $('.container');
+$p.appendTo($container);
+
+$container.append($msg);
+
+setTimeout(function () {
+  $msg.addClass('error');
+}, 5000);
+
+$('<span>', {
+  id: 'example',
+  text: 'ParolaMea',
+}).appendTo($msg);
+
+$('<h2>', {
+  text: 'Mesaje',
+});
